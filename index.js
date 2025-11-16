621299753264d159b5d5f692e005bfced9973f6d
Vaibhav Jha <vaibhavjha22@gmail.com>
2025-11-16 16:28:14 +0530

    refactor: fix child_process import, improve error handling, and enhance commitTool function for better output

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 import { Agent, run, tool } from "@openai/agents";
 import z from "zod";
-import execSync from "child_process";
+import { execSync } from "child_process";
 import "dotenv/config";
 
 async function getDiff() {
   try {
-    return execSync("git diff --staged", { encoding: "utf-8" });
+    const diff = execSync("git diff --staged", { encoding: "utf-8" });
+    return diff;
   } catch (error) {
-    console.error;
+    console.error("Error getting git diff:", error);
   }
 }
 
 const commitTool = tool({
   name: "commit-tool",
   description:
-    "An intelligent tool that transforms raw Git diffs into clear, concise, and meaningful commit messages. Analyzes code changes and automatically generates commit messages that follow best-practice conventions such as Conventional Commits",
+    "An intelligent tool that transforms raw Git diffs into clear, concise, and meaningful commit messages. Analyzes code changes and automatically generates commit messages that follow best-practice conventions such as Conventional Commits. Commit message should only be one line.",
   parameters: z.object({}),
-  execute: async (message) => {
-    // Simulate git commit operation
+  execute: async (m) => {
     const diff = await getDiff();
     console.log("Staged Changes:\n", diff);
-    return `Committed changes with message: "${message}"`;
+    return diff;
   },
 });
 
@@ -32,6 +32,7 @@ const gitAgent = new Agent({
 
 async function main(q = "") {
   const result = run(gitAgent, q);
+  console.log((await result).finalOutput);
 }
 
 main("Create a commit message for the staged changes.");
